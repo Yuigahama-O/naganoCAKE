41bc445886b9c2665fd3f44bc6b41715891be05a
EC2 Default User <ec2-user@ip-172-31-0-201.ec2.internal>
2022-06-18 05:54:19 +0000

    [add]admin_item_new

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+    has_one_attached :item_image
+    
+    def get_item_image(width, height)
+        item_image.variant(resize_to_limit: [width, height]).processed
+    end
+    
 end
