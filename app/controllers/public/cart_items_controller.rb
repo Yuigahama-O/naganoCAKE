a7b49370b70f0743e35617af1a27242622dbc836
EC2 Default User <ec2-user@ip-172-31-11-119.ec2.internal>
2022-06-19 05:12:53 +0000

    [途中]cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,13 +6,19 @@ class Public::CartItemsController < ApplicationController
 
   def update
     cart_item = CartItems.find(params[:id])
-    cart_item.update
+    cart_item.update(cart_item_params)
     redirect_to public_cart_items_path
   end
 
   def create
-    item = Item.find(cart_item_params[:item_id])
-    item.save
+    if @cart_item = CartItem.find_by(item_id: cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item = CartItem.save
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
+    end
     redirect_to public_cart_items_path
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
     has_one_attached :item_image
-    
+
+    has_many :cart_items, dependent: :destroy
+
     def get_item_image(width, height)
         item_image.variant(resize_to_limit: [width, height]).processed
     end
-    
+
     def with_tax_price
         (price * 1.1).floor
     end
-    
+
 end
