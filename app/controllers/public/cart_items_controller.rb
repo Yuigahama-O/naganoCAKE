971b3b17ac6ad6bbdb83d9ec81eafe342862646b
EC2 Default User <ec2-user@ip-172-31-11-119.ec2.internal>
2022-06-19 10:38:37 +0000

    [finish except layout]cart

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,19 +1,22 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items
     @sum
   end
 
   def update
-    cart_item = CartItems.find(params[:id])
+    cart_item = current_customer.cart_items.find(params[:id])
     cart_item.update(cart_item_params)
     redirect_to public_cart_items_path
   end
 
   def create
-    if @cart_item = CartItem.find_by(item_id: cart_item_params)
+    if @cart_item = CartItem.find_by(item_id: cart_item_params[:item_id])
       @cart_item.customer_id = current_customer.id
-      @cart_item = CartItem.save
+
+      @add_amount = @cart_item.amount + (params[:cart_item][:amount]).to_i
+      @cart_item.update(amount: @add_amount)
+
     else
       @cart_item = CartItem.new(cart_item_params)
       @cart_item.customer_id = current_customer.id
@@ -23,12 +26,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(cart_item_params[:item_id])
-    item.destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to public_cart_items_path
   end
 
   def destroy_all
-    CartItems.destroy_all
+    current_customer.cart_items.destroy_all
     redirect_to public_cart_items_path
   end
 
@@ -38,5 +42,7 @@ class Public::CartItemsController < ApplicationController
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
+
+
 
 end
