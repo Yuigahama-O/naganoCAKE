8f0fef9f2169ab5c80553f4774ac1c0e04037d31
EC2 Default User <ec2-user@ip-172-31-0-201.ec2.internal>
2022-06-20 08:22:39 +0000

    [add]admin_item_edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,9 +8,20 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(admin_item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
   end
   
   def create
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
     has_one_attached :item_image
 
     has_many :cart_items, dependent: :destroy
-
+    has_many :order_details, dependent: :destroy
     def get_item_image(width, height)
         item_image.variant(resize_to_limit: [width, height]).processed
     end
