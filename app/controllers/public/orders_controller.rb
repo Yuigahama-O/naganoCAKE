3aaeb14180dadfd0e79bc82962181190e6b4b4a0
EC2 Default User <ec2-user@ip-172-31-11-119.ec2.internal>
2022-06-20 10:11:02 +0000

    [temporary]order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,8 @@
 class Public::OrdersController < ApplicationController
+
+
   def new
+    @order = current_customer.orders.new
   end
 
   def index
@@ -8,6 +11,40 @@ class Public::OrdersController < ApplicationController
   def show
   end
 
+  def comfirm
+    @order = Order.new(order_params)
+
+
+    if params[:order][:address_option] == "my_address"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+      @order.save
+      redirect_to public_order_path(@order)
+    elsif params[:order][:address_option] == "choose_address"
+      address = current_customer.deliveries.find(params[:order][:delivery_id])
+      @order.postal_code = address.postal_code
+      @order.address = address.address
+      @order.name = address.name
+      @order.save
+      redirect_to public_order_path(@order)
+    else
+      @order.invalid?
+      render 'new'
+    end
+
+  end
+
+  def create
+
+  end
+
   def complete
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :name, :address)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ApplicationRecord
+
+  belongs_to :customer
+  has_many :order_details, dependent: :destroy
+
+  validates :postal_code, :name, :address, presence: true
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+
+
 end
