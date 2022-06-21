4bd6c1a66690e67c666364c856f8e299b6902ac7
EC2 Default User <ec2-user@ip-172-31-0-201.ec2.internal>
2022-06-21 04:11:46 +0000

    Y-delivery

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,7 +1,38 @@
 class Public::DeliveriesController < ApplicationController
   def index
+    @deliveries = Delivery.all
   end
 
   def edit
+    @delivery = Delivery.find(params[:id])
   end
+  
+  def create
+    @delivery = Delivery.new(deliveries_params)
+    @delivery.save
+    redirect_to public_deliveries_path
+  end
+  
+  def destroy
+    @delivery = Delivery.find(params[:id])
+    @delivery.destroy
+    redirect_to public_deliveries_path
+  end
+  
+  def update
+    @delivery = Delivery.find(params[:id])
+    @delivery.update(delivery_params)
+    redirect_to public_deliveries_path
+  end
+  
+  private
+  def delivery_params
+    params.require(:delivery).permit(:customer_id, :postal_code, :address, :name)
+  end
+  
+  def deliveries_params
+    params.permit(:customer_id, :postal_code, :address, :name)
+  end
+  
+  
 end
