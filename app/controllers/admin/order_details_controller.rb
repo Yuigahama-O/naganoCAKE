5f4b5b4005766b6a44a4eadfba1aeee295c8455a
EC2 Default User <ec2-user@ip-172-31-74-27.ec2.internal>
2022-06-26 07:34:28 +0000

    headerの追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,5 @@
 class Admin::OrderDetailsController < ApplicationController
+  before_action :authenticate_admin!, except: [:top,:about]
 
   def update
     @order_detail = OrderDetail.find(params[:id])
