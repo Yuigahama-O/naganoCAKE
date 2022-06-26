708f9f8514a30cc4f980b0d42e06ca03f51b5efe
EC2 Default User <ec2-user@ip-172-31-74-27.ec2.internal>
2022-06-26 06:43:39 +0000

    header整理、customer_orderリダイレクト

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,5 @@
 class Public::CartItemsController < ApplicationController
+      before_action :authenticate_customer!, except: [:top,:about]
   def index
     @cart_items = current_customer.cart_items
     @sum
